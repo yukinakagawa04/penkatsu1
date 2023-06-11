7b9d45f6fb6f9cb54ad7ee5a8ce12d8c9df8a883
yukinakagawa04 <9704yn@gmail.com>
2023-06-11 12:53:16 +0000

    0611

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,19 +33,20 @@
                     <div>
                         <h1>ペン活</h1>
                     </div>
-                    @if (Route::has('login'))
-                        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                            @auth
-                                <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">ホーム</a>
-                            @else
-                                <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">ログイン</a>
-        
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">ユーザー登録</a>
-                                @endif
-                            @endauth
-                        </div>
-                    @endif
+                    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+                        @if (Route::has('login'))
+                            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                                @auth
+                                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">ホーム</a>
+                                @else
+                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">ログイン</a>
+                                    @if (Route::has('register'))
+                                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">ユーザー登録</a>
+                                    @endif
+                                @endauth
+                            </div>
+                        @endif
+                    </nav>
                 </div>
             </div>
         </div>
