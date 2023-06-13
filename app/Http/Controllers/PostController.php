d93faa0eb19cea9b9594a4ce97ce7110e4ae4aab
yukinakagawa04 <9704yn@gmail.com>
2023-06-13 10:58:09 +0000

    0613

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,7 +20,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(5);
+        $posts = Post::orderBy('created_at', 'desc')->paginate(5);
         return view('post.index', compact('posts'));
     }
 
